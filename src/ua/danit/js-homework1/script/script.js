400cd9a5cb6963ac0d687b772d9989ab237da74f
yamnyk <notAunicorN2018>
2018-08-20 19:08:49 +0300

    Initial commit

---
+++ src/ua/danit/js-homework1/script/script.js
@@ -0,0 +1,9 @@
+let credentials = prompt('Enter your Name and Age. \n Example: Ivan 42').split(' ');
+let age = +credentials[1];
+if(age < 18){
+    alert('You are not allowed to visit this website');
+} else if(18 <= age <= 22){
+    confirm('Are you sure you want to continue?') ? alert(`Welcome, ${credentials[0]}`): alert('You are not allowed to visit this website');
+} else if(age > 22){
+    alert(`Welcome, ${credentials[0]}`)
+}
