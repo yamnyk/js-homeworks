4154aa726addcf77b3eebfe8e33e13458d370c84
yamnyk <notAunicorN2018>
2018-08-30 17:59:48 +0300

    homework8 | finding way to build some structure

--- src/ua/danit/js-homework8/script/script.js
+++ src/ua/danit/js-homework8/script/script.js
@@ -1,10 +1,34 @@
-var Car = {
-    isMachine: true
+function filterCollection(inputArray, keyWords, flag) {
+     let keyWordsArray = keyWords.split(' ').map(word => word.toLowerCase());
+     let properties = [].slice.call(arguments, 3, arguments.length).map(word => word.toLowerCase());
+     console.log(keyWordsArray);
+     console.log(properties);
+    /**
+     * перебираю каждое ключевое слово. Для каждого ключевого слова запускаю поиск по каждой из
+     * свойств каждого из объектов. Если нашел хоть одно совпадение - возвращаю true для этого обхекта в фильр;
+     * если совпадений в значениях свойства этого объекта не нашлось - возвращаю false для фильтра
+     */
+/
+    function findProp(item) {
+        for(prop in item){
+            if(properties.includes(prop)){
+                 return prop
+            } else if(typeof prop === 'object') {
+                 findProp(prop)
+            }
+        }
+    }
+    return inputArray.filter(function(el, ind, array){
+          if(flag){
+
+          } else {
+               return inputArray.some(function (item) {
+                   for(some in findProp(item)){
+
+                   }
+               })
+          }
+     })
 }
 
-function BMV(model) {
-    this.model = model;
-}
-BMV.prototype = Car;
-
-new BMV("Toyota").is
+console.log(filterCollection([],'Pop Lola gOgi ufufF', false,'Gogi', 'mogi', 'dodik'));
