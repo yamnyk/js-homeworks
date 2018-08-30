85d06481666296000727df51e9fe1426f95e4ba7
yamnyk <notAunicorN2018>
2018-08-31 01:14:42 +0300

    homework8 | includesIntoProps() works

--- src/ua/danit/js-homework8/script/script.js
+++ src/ua/danit/js-homework8/script/script.js
@@ -2,8 +2,8 @@ function filterCollection(inputArray, keyWords, flag) {
     let keyWordsArray = keyWords.split(' ').map(word => word.toLowerCase());
     let propertiesToSearch = [].slice.call(arguments, 3, arguments.length).map(word => word.toLowerCase());
     console.log(keyWordsArray);
-    console.log(properties);
-/
+    console.log(propertiesToSearch);
+
     return inputArray.filter(function(el, ind, array){
         if(flag){
             return findMatchesInPropsALL(el, propertiesToSearch, keyWords);
@@ -15,14 +15,26 @@ function filterCollection(inputArray, keyWords, flag) {
 
 function includesIntoProps(el, property) {
     if(property.indexOf('.') === -1){
-        return el[property] !== undefined;
+        return el[property] !== undefined ? el[property] : false;
     } else {
         let propList = property.split('.');
         //TODO: how I cat get recursively into last prop of the chain...
         //smth like this `[${prop.split('.').join('][')}]`
+        let tempObj = obj;
+        for (let i = 0; i < propList.length; i++) {
+            if(i === propList.length-1 && tempObj[propList[i]] !== undefined) {
+                return tempObj[propList[i]];
+            } else if(typeof tempObj === 'object'){
+                if(tempObj[propList[i]] === undefined) {
+                    return false
+                } else {
+                    tempObj = tempObj[propList[i]]
+                }
+            } else if(typeof tempObj !== 'object') {
+                return false
+            }
+        }
     }
-
-    return undefined;
 }
 
 function findMatchesInPropsALL(el, propertiesToSearch, keyWords) {
