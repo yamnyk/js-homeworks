d62aa83414320460c7b4c5da3cf2f4bc6fc2629f
yamnyk <notAunicorN2018>
2018-08-30 20:05:28 +0300

    homework8 | pseudokoding the algorithm

--- src/ua/danit/js-homework8/script/script.js
+++ src/ua/danit/js-homework8/script/script.js
@@ -1,34 +1,53 @@
 function filterCollection(inputArray, keyWords, flag) {
-     let keyWordsArray = keyWords.split(' ').map(word => word.toLowerCase());
-     let properties = [].slice.call(arguments, 3, arguments.length).map(word => word.toLowerCase());
-     console.log(keyWordsArray);
-     console.log(properties);
-    /**
-     * перебираю каждое ключевое слово. Для каждого ключевого слова запускаю поиск по каждой из
-     * свойств каждого из объектов. Если нашел хоть одно совпадение - возвращаю true для этого обхекта в фильр;
-     * если совпадений в значениях свойства этого объекта не нашлось - возвращаю false для фильтра
-     */
+    let keyWordsArray = keyWords.split(' ').map(word => word.toLowerCase());
+    let propertiesToSearch = [].slice.call(arguments, 3, arguments.length).map(word => word.toLowerCase());
+    console.log(keyWordsArray);
+    console.log(properties);
 /
-    function findProp(item) {
-        for(prop in item){
-            if(properties.includes(prop)){
-                 return prop
-            } else if(typeof prop === 'object') {
-                 findProp(prop)
-            }
-        }
-    }
     return inputArray.filter(function(el, ind, array){
-          if(flag){
+        if(flag){
+            return findMatchesInPropsALL(el, propertiesToSearch, keyWords);
+        } else {
+            return findMatchesInPropsANY(el, propertiesToSearch, keyWords);
+        }
+    })
+}
 
-          } else {
-               return inputArray.some(function (item) {
-                   for(some in findProp(item)){
+function includesIntoProps(el, property) {
+    if(property.indexOf('.') === -1){
+        return el[property] !== undefined;
+    } else {
+        let propList = property.split('.');
+        //TODO: how I cat get recursively into last prop of the chain...
+        //smth like this `[${prop.split('.').join('][')}]`
+    }
 
-                   }
-               })
-          }
-     })
+    return undefined;
+}
+
+function findMatchesInPropsALL(el, propertiesToSearch, keyWords) {
+    let counterOfMatchedKeywords = 0;
+
+    //FOR EVERY PROPERTY
+        //find out if this property includes into propertiesToSearch with includesIntoProps()
+        //if FALSE: next property
+        //if TRUE:
+            //FOR EVERY KEYWORD
+            //find out if this property include this keyword
+                //if TRUE: counterOfMatchedKeywords++
+                //if FALSE: next keyword
+
+    return counterOfMatchedKeywords === keyWords.length;
+}
+
+function findMatchesInPropsANY(el, propertiesToSearch, keyWords) {
+    //FOR EVERY PROPERTY:
+        // find out if this includes into propertiesToSearch with includesIntoProps()
+        //TRUE: find any matches with keyWords array
+                    //if its match - return true
+                    //if there is NO matches - return false
+        //FALSE: return false
+    return undefined;
 }
 
 console.log(filterCollection([],'Pop Lola gOgi ufufF', false,'Gogi', 'mogi', 'dodik'));
