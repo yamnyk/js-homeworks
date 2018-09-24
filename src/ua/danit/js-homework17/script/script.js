63452f1e78df91376dce1b6c539f30ec5c9f8da0
yamnyk <notAunicorN2018>
2018-09-24 16:48:04 +0300

    homework17 | done

--- src/ua/danit/js-homework17/script/script.js
+++ src/ua/danit/js-homework17/script/script.js
@@ -1,10 +1,50 @@
-$('#inputSize').hide();
-$('#btn-sbmt').hide();
-
-$('#first-btn').click(function () {
-    $(this).hide();
-    $('#inputSize').fadeIn();
-    $('#btn-sbmt').fadeIn();
-    // alert('Gogi hates you!');
+$(document).ready(function () {
+   randomColor();
+   circlesMess();
 });
+
+function randomColor() {
+    let num1 = Math.floor(Math.random() * 256)+1;
+    let num2 = Math.floor(Math.random() * 256)+1;
+    let num3 = Math.floor(Math.random() * 256)+1;
+    let num4 = Math.random()+0.3;
+    return `${num1}, ${num2}, ${num3}, ${num4}`;
+}
+
+function circlesMess(){
+    $('#inputSize').hide();
+    $('#btn-sbmt').hide();
+
+    $('#first-btn').click(function () {
+        $(this).hide();
+        $('#inputSize').fadeIn();
+        $('#btn-sbmt').fadeIn();
+    });
+
+    $('#btn-sbmt').click(function () {
+        let radius = $('#inputSize')[0].value;
+        let circles = $(document.createDocumentFragment());
+        let arrayOfCircles = [];
+        for (let i = 0; i < 100; i++) {
+            $('#circles').append($(document.createElement('div')).addClass('gogi').css({
+                display : 'inline-block',
+                width: `${radius}px`,
+                height: `${radius}px`,
+                backgroundColor: `rgba(${randomColor()})`,
+                borderRadius: '50%',
+                margin: '10px',
+                opacity: 1,
+                transition : 'opacity 0.2s'
+            }));
+        }
+        $('#btn-sbmt').fadeOut();
+    });
+
+    $('#circles').click(function (e) {
+        if(e.target.classList.contains('gogi')){
+            e.target.style.opacity = 0;
+        }
+    });
+}
+
 
