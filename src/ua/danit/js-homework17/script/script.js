b04b5901c3027a0b52415a62edea285c13278bd7
yamnyk <notAunicorN2018>
2018-09-24 12:45:59 +0300

    homework17 | add buttons and input shows and hides by jQuery

---
+++ src/ua/danit/js-homework17/script/script.js
@@ -0,0 +1,10 @@
+$('#inputSize').hide();
+$('#btn-sbmt').hide();
+
+$('#first-btn').click(function () {
+    $(this).hide();
+    $('#inputSize').fadeIn();
+    $('#btn-sbmt').fadeIn();
+    // alert('Gogi hates you!');
+});
+
